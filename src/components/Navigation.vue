f26d6960344cb0e69bc82a3926f8285a922b8905
hhaml <hakim.hamlat.1@ulaval.ca>
2018-09-26 14:13:51 -0400

    premier test commit

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,18 +1,36 @@
 <template>
-    <nav>
-        <ul>
-            <li>
-                <router-link to="/">Home</router-link>
-            </li>
-            <li>
-                <router-link to="/album">Album</router-link>
-            </li>
-            <li>
-                <router-link to="/artist">Artist</router-link>
-            </li>
-        </ul>
-    </nav>
+  <nav>
+    <div id="menu">
+      <ul>
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/album">Album</router-link>
+        </li>
+        <li>
+          <router-link to="/artist">Artist</router-link>
+        </li>
+        <li>
+          <router-link to="/artist">Artist</router-link>
+        </li>
+        <li>
+          <router-link to="/artist">Artist</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
+
 <style>
+  #menu{
+    width: 20%;
+    background-color: red;
+    float: left;
+    height: 20%;
+  }
+  #menu li:hover {
+    background-color: beige; /* Dark grey background on mouse-over */
+  }
 </style>
